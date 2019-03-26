6865e58aa2c9c60b1fd3a9ac5576bf0708a65584
HowellYang <th15817161961@gmail.com>
2019-03-26 09:59:56 +0800

    init

--- middleware/mysql-cluster/mysql-cluster-atomscat/build&push.sh
+++ middleware/mysql-cluster/mysql-cluster-atomscat/build&push.sh
@@ -2,3 +2,5 @@
 docker build -t mysql-cluster-atomscat .;
 docker tag mysql-cluster-atomscat:latest 192.168.31.148/middleware/mysql-cluster-atomscat:latest
 docker push 192.168.31.148/middleware/mysql-cluster-atomscat:latest
+kubectl delete -f ../k8s_ip.yaml
+kubectl apply -f ../k8s_ip.yaml
